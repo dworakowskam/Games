7ad2f552c0c87dd7efb89d32702f4c791cfd1dee
dworakowskam <112777097+dworakowskam@users.noreply.github.com>
2022-12-15 21:20:59 +0100

    Update tic_tac_toe_2.py

--- tic_tac_toe_GUI/tic_tac_toe_2.py
+++ tic_tac_toe_GUI/tic_tac_toe_2.py
@@ -1,16 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 13 20:02:08 2022
-
-@author: MD
-"""
-
-
 # import the classic Tk themed widgets
 import tkinter as tk
 # import the new Tk themed widgets
 from tkinter import ttk
-from tkinter.messagebox import showinfo
+
 
 # fix blurry text and UI
 from ctypes import windll 
@@ -28,8 +20,8 @@ root.iconbitmap("favicon.ico")
 
 
 # determine window size and center the window on the screen
-window_width = 900
-window_height = 900
+window_width = 500
+window_height = 400
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 center_x = int(screen_width/2 - window_width/2)
@@ -45,28 +37,30 @@ message = ttk.Label(
     background="cornsilk2",
     relief="solid",
     justify=tk.CENTER,
-    padding=10,
+    padding=20,
     font=("Verdana", 9)
 )
 message.pack()
 
 
 # create Button widgets for a play
+ipadding = {"ipadx": 5, "ipady": 40}
 def return_pressed(event):
     print("Return key pressed.")
 photo_x = tk.PhotoImage(file="x.png")
 button1 = ttk.Button(root)
 button1.bind('<Button>', return_pressed)
 button1.focus()
-button1.pack()
+button1.pack(side=tk.LEFT, expand=True, **ipadding)
 button2 = ttk.Button(root)
 button2.bind('<Button>', return_pressed)
 button2.focus()
-button2.pack()
+button2.pack(side=tk.LEFT, expand=True, **ipadding)
 button3 = ttk.Button(root)
 button3.bind('<Button>', return_pressed)
 button3.focus()
-button3.pack()
+button3.pack(side=tk.LEFT, expand=True, **ipadding)
+
 
 # # set the disabled flag
 # button.state(['disabled'])
@@ -80,7 +74,7 @@ image_label_example = ttk.Label(
     image=photo_o,
     padding=5
 )
-image_label_example.pack()
+#image_label_example.pack(side=tk.LEFT)
 
 
 # # exit button PROGRAM SIĘ WIESZA!!!
