d82ad748f1cbc808782087c40c4083e5775ae825
dworakowskam <112777097+dworakowskam@users.noreply.github.com>
2022-12-17 17:58:46 +0100

    Update tic_tac_toe_2.py

--- tic_tac_toe_GUI/tic_tac_toe_2.py
+++ tic_tac_toe_GUI/tic_tac_toe_2.py
@@ -20,8 +20,8 @@ root.iconbitmap("favicon.ico")
 
 
 # determine window size and center the window on the screen
-window_width = 432
-window_height = 493
+window_width = 437
+window_height = 497
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 center_x = int(screen_width/2 - window_width/2)
@@ -42,68 +42,77 @@ message = ttk.Label(
 message.grid(row=0, columnspan=3, ipadx=15, ipady=35)
 
 
-# create Button widgets for a play
+# create Button widgets and frames for play
 ipadding = {"ipadx": 12, "ipady": 45}
 def return_pressed(event):
     print("Return key pressed.")
     
-button1 = ttk.Button(root)
+frame1 = ttk.Frame(root, borderwidth=1, relief="ridge")
+frame1.grid(row=1, column=0, sticky=tk.SE)    
+button1 = ttk.Button(frame1)
 button1.bind('<Button>', return_pressed)
-button1.focus()
-button1.grid(row=1, column=0, **ipadding, sticky=tk.SE)
+button1.grid(**ipadding)
 
-button2 = ttk.Button(root)
+frame2 = ttk.Frame(root, borderwidth=1, relief="ridge")
+frame2.grid(row=1, column=1)  
+button2 = ttk.Button(frame2)
 button2.bind('<Button>', return_pressed)
-button2.focus()
-button2.grid(row=1, column=1, **ipadding)
+button2.grid(**ipadding)
 
-button3 = ttk.Button(root)
+frame3 = ttk.Frame(root, borderwidth=1, relief="ridge")
+frame3.grid(row=1, column=2, sticky=tk.W)  
+button3 = ttk.Button(frame3)
 button3.bind('<Button>', return_pressed)
-button3.focus()
-button3.grid(row=1, column=2, **ipadding, sticky=tk.W)
+button3.grid(**ipadding)
 
-button4 = ttk.Button(root)
+frame4 = ttk.Frame(root, borderwidth=1, relief="ridge")
+frame4.grid(row=2, column=0, sticky=tk.E) 
+button4 = ttk.Button(frame4)
 button4.bind('<Button>', return_pressed)
-button4.focus()
-button4.grid(row=2, column=0, **ipadding, sticky=tk.E)
+button4.grid(**ipadding)
 
-button5 = ttk.Button(root)
+frame5 = ttk.Frame(root, borderwidth=1, relief="ridge")
+frame5.grid(row=2, column=1) 
+button5 = ttk.Button(frame5)
 button5.bind('<Button>', return_pressed)
-button5.focus()
-button5.grid(row=2, column=1, **ipadding)
+button5.grid(**ipadding)
 
-button6 = ttk.Button(root)
+frame6 = ttk.Frame(root, borderwidth=1, relief="ridge")
+frame6.grid(row=2, column=2, sticky=tk.W) 
+button6 = ttk.Button(frame6)
 button6.bind('<Button>', return_pressed)
-button6.focus()
-button6.grid(row=2, column=2, **ipadding, sticky=tk.W)
+button6.grid(**ipadding)
 
-button7 = ttk.Button(root)
+frame7 = ttk.Frame(root, borderwidth=1, relief="ridge")
+frame7.grid(row=3, column=0, sticky=tk.NE) 
+button7 = ttk.Button(frame7)
 button7.bind('<Button>', return_pressed)
-button7.focus()
-button7.grid(row=3, column=0, **ipadding, sticky=tk.NE)
+button7.grid(**ipadding)
 
-button8 = ttk.Button(root)
+frame8 = ttk.Frame(root, borderwidth=1, relief="ridge")
+frame8.grid(row=3, column=1, sticky=tk.NE) 
+button8 = ttk.Button(frame8)
 button8.bind('<Button>', return_pressed)
-button8.focus()
-button8.grid(row=3, column=1, **ipadding)
+button8.grid(**ipadding)
 
-button9 = ttk.Button(root)
+frame9 = ttk.Frame(root, borderwidth=1, relief="ridge")
+frame9.grid(row=3, column=2, sticky=tk.NW) 
+button9 = ttk.Button(frame9)
 button9.bind('<Button>', return_pressed)
-button9.focus()
-button9.grid(row=3, column=2, **ipadding, sticky=tk.NW)
+button9.grid(**ipadding)
 
-# # set the disabled flag
-# button.state(['disabled'])
-# # remove the disabled flag
-# button.state(['!disabled'])
+# # # set the disabled flag
+# # button.state(['disabled'])
+# # # remove the disabled flag
+# # button.state(['!disabled'])
 
-photo_x = tk.PhotoImage(file="x.png")
-photo_o = tk.PhotoImage(file="o.png")
-image_label_example = ttk.Label(
-    root,
-    image=photo_o,
-    padding=5
-)
+# photo_x = tk.PhotoImage(file="x.png")
+# photo_o = tk.PhotoImage(file="o.png")
+# image_label_example = ttk.Label(
+#     frame,
+#     image=photo_o,
+#     padding=5
+# )
 #image_label_example.pack(side=tk.LEFT)
 
 
